281d4ea4f7051d05027cf8f81e04e33a305ca5ae
William Xi <william.xi@samsung.com>
2020-07-09 14:08:25 -0700

    changed directories

--- test_voice_cloning.py
+++ test_voice_cloning.py
@@ -19,20 +19,20 @@ if "utils" in sys.modules:
 
 from synthesize import synthesize
 from utils import build_model
-os.chdir(os.path.join(os.path.expanduser("~"), tacotron_dir))
-model = build_model(os.path.join(os.path.expanduser("~"), "checkpoints", tacotron_chpt))
+os.chdir(os.path.join(os.getcwd(), tacotron_dir))
+model = build_model(os.path.join(os.path.dirname(os.getcwd()), "checkpoints", tacotron_chpt))
 model.eval()
 
 spectrograms = [synthesize(model, "|" + i) for i in inputs]
 #print(spectrograms[0].shape)
 
-os.chdir(os.path.join(os.path.expanduser("~"), wavernn_dir))
+os.chdir(os.path.join(os.path.join(os.path.dirname(os.getcwd()), wavernn_dir))
 if "utils" in sys.modules:
     del sys.modules["utils"]
 
 print(os.getcwd())
 
-sys.path.insert(0, os.path.join(os.path.expanduser("~"), wavernn_dir))
+sys.path.insert(0, os.getcwd())
 from models.fatchord_version import WaveRNN
 from utils import hparams as hp
 from gen_wavernn import generate
@@ -42,14 +42,14 @@ hp.configure('hparams.py')
 model = WaveRNN(rnn_dims=hp.voc_rnn_dims, fc_dims=hp.voc_fc_dims, bits=hp.bits, pad=hp.voc_pad, upsample_factors=hp.voc_upsample_factors,
                 feat_dims=hp.num_mels, compute_dims=hp.voc_compute_dims, res_out_dims=hp.voc_res_out_dims, res_blocks=hp.voc_res_blocks,
                 hop_length=hp.hop_length, sample_rate=hp.sample_rate, mode=hp.voc_mode).to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-model.load(os.path.join(os.path.expanduser("~"), "checkpoints", wavernn_chpt))
+model.load(os.path.join(os.path.join(os.path.dirname(os.getcwd()) "checkpoints", wavernn_chpt))
 
 waveforms = [generate(model, s, hp.voc_gen_batched, hp.voc_target, hp.voc_overlap) for s in spectrograms]
 
 from scipy.io.wavfile import write
 import numpy as np
 
-os.chdir(os.path.join(os.path.expanduser("~"), "audiofiles"))
+os.chdir(os.path.join(os.path.join(os.path.dirname(os.getcwd()), "audiofiles"))
 for idx, w in enumerate(waveforms):
   print(inputs[idx])
   print(w)
