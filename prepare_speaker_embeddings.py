7304fe50f7ff60a07afcbbe843a5dfa40ad206e4
William Xi <william.xi@samsung.com>
2020-07-21 12:49:13 -0700

    totensor

--- prepare_speaker_embeddings.py
+++ prepare_speaker_embeddings.py
@@ -13,7 +13,7 @@ def getSpeakerEmbeddings(speaker_ids):
             speaker = line[1]
             if speaker in speaker_ids:
                 speaker_index = speaker_ids.index(speaker)
-                if len(speakerUtterances[speaker_index]) < 250:
+                if len(speakerUtterances[speaker_index]) < 50:
                     wavpath = line[3]
                     speakerUtterances[speaker_index].append(wavpath)
 
@@ -31,6 +31,7 @@ def getSpeakerEmbeddings(speaker_ids):
             speakerEmbeddings[i] += np.asarray(encoder.embed_utterance(wav))
         speakerEmbeddings[i] /= len(speaker_wavs)
     speakerEmbeddings = np.asarray(speakerEmbeddings)
+    speakerEmbeddings = torch.tensor(speakerEmbeddings)
     return speakerEmbeddings
 
 
