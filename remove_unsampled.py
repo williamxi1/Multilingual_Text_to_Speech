fc58efb879c984f720823ec4f92418cc971392e5
William Xi <william.xi@samsung.com>
2020-07-29 08:09:38 -0700

    asdf

--- remove_unsampled.py
+++ remove_unsampled.py
@@ -1,13 +1,10 @@
 import os
 import subprocess
 
-os.chdir('data/css10/spanish/slr72/wavsfemale')
+os.chdir('data/css10/chinese/data_thchs30/data')
 
 print("REMOVING")
 # subprocess.run(['find . -name "*.bak" -type f -delete'])
 for i in range(10):
     subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
-os.chdir('../wavsmale')
 
-for i in range(10):
-    subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
