7e536894bdf031f8607800621b2c0e86e3fb1a29
William Xi <william.xi@samsung.com>
2020-07-29 08:04:51 -0700

    asdf

--- remove_unsampled.py
+++ remove_unsampled.py
@@ -1,17 +1,13 @@
 import os
 import subprocess
 
-os.chdir('data/css10/english/VCTK-Corpus/wavs')
+os.chdir('data/css10/spanish/slr72/wavsfemale')
 
-speakers = str(subprocess.check_output(['ls']))
-speakers = speakers[2:-3].split('\\n')
+print("REMOVING")
+# subprocess.run(['find . -name "*.bak" -type f -delete'])
+for i in range(10):
+    subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
+os.chdir('../wavsmale')
 
-print(speakers)
-
-for speaker in speakers:
-    os.chdir(speaker)
-    print("REMOVING")
-   # subprocess.run(['find . -name "*.bak" -type f -delete'])
-    for i in range(10):
-        subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
-    os.chdir('..')
+for i in range(10):
+    subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
