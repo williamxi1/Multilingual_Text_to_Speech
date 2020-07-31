19c57a650da7d71db58705a404693073558b7b8a
William Xi <william.xi@samsung.com>
2020-07-30 19:24:33 -0700

    asdf

--- remove_unsampled.py
+++ remove_unsampled.py
@@ -6,9 +6,6 @@ speakers = str(subprocess.check_output(['ls']))
 speakers = speakers[2:-3].split('\\n')
 print("REMOVING")
 # subprocess.run(['find . -name "*.bak" -type f -delete'])
-for speaker in speakers:
-    os.chdir(speaker)
     #ubprocess.run(['for', 'i', 'in', '*wav;','do', 'echo', '$i;', 'sox', '$i', '-r', '22050', '${i%%.wav}r.wav; done])
-    for i in range(10):
-        subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
-    os.chdir('..')
+for i in range(10):
+    subprocess.run(['find', '.', '-name', '*' + str(i) + '.wav', '-type', 'f', '-delete'])
