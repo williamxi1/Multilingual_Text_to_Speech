096f4e8c358e7099e51f55d36129796a4255d4e5
William Xi <william.xi@samsung.com>
2020-08-14 09:31:34 -0700

    asdf

--- prepare_speaker_embeddings.py
+++ prepare_speaker_embeddings.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 def getSpeakerEmbeddings(speaker_ids):
-
+    print("Obtaining Embeddings")
     if os.path.exists('speakerEmbeddings.npy'):
         print("Loading Pre-existing Embeddings")
         speakerEmbeddings = np.load('speakerEmbeddings.npy')
